deaf1356194ae86d5441227c34be8be5e910c298
yuz168 <f.kokori85@gmail.com>
2025-05-18 15:29:44 +0900

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    password_id TEXT,  -- パスワードから生成したIDカラムを追加
-    text TEXT NOT NULL,
-    created_at DATETIME NOT NULL
-);
